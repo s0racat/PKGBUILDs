bfa10ef28a191989b408167d3ef116a9bf9fbead
soracqt <72482919+soracqt@users.noreply.github.com>
2022-02-07 08:13:50 +0900

    update

--- slack-electron/slack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-exec electron --enable-accelerated-mjpeg-decode --enable-accelerated-video --ignore-gpu-blacklist --enable-native-gpu-memory-buffers --enable-gpu-rasterization /usr/lib/slack/app.asar "$@"
-
